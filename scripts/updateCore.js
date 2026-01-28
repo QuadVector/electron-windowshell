04004d2ed9d11ee097567060461ff06298d80809
Mihairu <66649825+QuadVector@users.noreply.github.com>
2026-01-29 00:58:22 +0300

    Update core (work in progress)

--- scripts/updateCore.js
+++ scripts/updateCore.js
@@ -1,4 +1,19 @@
 const https = require('https');
 const fs = require('fs');
 
+const downloadURL = "https://github.com/QuadVector/electron-fluent-template/raw/refs/heads/Dev/src/core/download.zip";
+const downloadPath = "./src/core/download.zip";
+
 console.log("Updating core...");
+
+//check is archive exists and then delet it if it is
+if(fs.existsSync(downloadPath)) {
+    fs.unlinkSync(downloadPath);
+}
+
+//download the archive
+console.log("Downloading archive...");
+https.get(downloadURL, (response) => {
+    const file = fs.createWriteStream(downloadPath);
+    response.pipe(file);
+});
